74d1b7d70aac9391d28f8becd05b1f4a103160a0
Samuele <samueletasciotti@icloud.com>
2024-09-11 00:58:16 +0200

    feat: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,71 @@
 <footer class="my-5 text-center">
-    <p>Footer</p>
+
+    <!-- List of Footer -->
+    <div class="ft-box" style="background-image: url('img/footer-bg.jpg')">
+
+        <!-- 1 -->
+        <div class="ul">
+            <ul>
+                <h4>DC COMICS</h4>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+
+            <ul>
+                <h4>SHOP</h4>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+        </div>
+
+        <!-- 2 -->
+        <div class="ul">
+            <ul>
+                <h4>DC</h4>
+                <li>Terms Of Use</li>
+                <li>Privicy Policy (New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscription</li>
+                <li>Talent Workshop</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+
+        <!-- 3 -->
+        <div class="ul">
+            <ul>
+                <h4>SITES</h4>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Univers</li>
+                <li>DC Power Visa</li>
+            </ul>
+        </div>
+    </div>
+
+    
+
+    <!-- Links -->
+    <div class="ft-link">
+
+        <!-- Button -->
+        <div class="button">
+
+        </div>
+
+        <!-- Link -->
+        <div class="link">
+
+        </div>
+    </div>
 </footer>
